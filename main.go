d5107021c96bda67957ff564fbb03c2815923e9b
BosBJJ <samgitrdone@gmail.com>
2026-09-05 15:50:30 -0500

    "basic server"

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Welcome)
+
+	fmt.Println("Server started on port 8080")
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func Welcome(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Volunteer App!")
+}
