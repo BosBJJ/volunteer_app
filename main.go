900acea63606f92d0eaffe8160bda0869c9ae0cd
BosBJJ <samgitrdone@gmail.com>
2026-09-07 18:31:19 -0500

    "add simple get/getbyID"

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -11,7 +12,8 @@ var volunteers []Volunteer
 
 func main() {
 	http.HandleFunc("/", Welcome)
-	http.HandleFunc("/volunteer", CreateVolunteer)
+	http.HandleFunc("/volunteer", VolunteerHandler)
+	http.HandleFunc("/volunteer/{id}", GetVolunteerByID)
 
 	fmt.Println("Server started on port 8080")
 
@@ -22,6 +24,20 @@ func Welcome(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "Welcome to the Volunteer App!")
 }
 
+func VolunteerHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		CreateVolunteer(w, req)
+	case http.MethodGet:
+		GetVolunteers(w, req)
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+// temporary until SQL implemented
+var nextID int = 0
+
 func CreateVolunteer(w http.ResponseWriter, req *http.Request) {
 	var volunteer Volunteer
 	err := json.NewDecoder(req.Body).Decode(&volunteer)
@@ -29,13 +45,41 @@ func CreateVolunteer(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
+	if volunteer.Name == "" || volunteer.Email == "" {
+		http.Error(w, "name and email are required", http.StatusBadRequest)
+		return
+	}
 	volunteer.RegisteredAt = time.Now()
+	volunteer.Id = nextID
 	volunteers = append(volunteers, volunteer)
 	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(volunteer)
+	nextID++
+}
+
+func GetVolunteerByID(w http.ResponseWriter, req *http.Request) {
+	path := req.PathValue("id")
+	reqID, err := strconv.Atoi(path)
+	if err != nil {
+		http.Error(w, "invalid input", http.StatusBadRequest)
+		return
+	}
+	for _, volunteer := range volunteers {
+		if volunteer.Id == reqID {
+			w.Header().Set("content-type", "application/json")
+			json.NewEncoder(w).Encode(volunteer)
+			return
+		}
+	}
+	http.Error(w, "volunteer not found", http.StatusNotFound)
+}
+func GetVolunteers(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(volunteers)
 }
 
 type Volunteer struct {
+	Id           int       `json:"id"`
 	Name         string    `json:"name"`
 	Email        string    `json:"email"`
 	RegisteredAt time.Time `json:"registeredAt"`
